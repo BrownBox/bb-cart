c41c53a5447143a9936d32bb0095d4669614cca0
Mark Parnell <markparnell@brownbox.net.au>
2018-06-26 10:09:39 +1000

    Fix override of confirmation redirects for donate/checkout forms

--- forms/confirmation.php
+++ forms/confirmation.php
@@ -1,10 +1,20 @@
 <?php
 add_filter('gform_confirmation', 'bb_cart_form_confirmation', 10, 3);
 function bb_cart_form_confirmation($confirmation, $form, $entry) {
-    if (in_array('bb_cart_donations', explode(' ', $form['cssClass'])) && class_exists('Brownbox\Config\BB_Cart') && isset(Brownbox\Config\BB_Cart::$donate_form_confirmation_url) && $confirmation['type'] == 'redirect') {
-        $confirmation['url'] = site_url(Brownbox\Config\BB_Cart::$donate_form_confirmation_url);
-    } elseif (in_array('bb_cart_checkout', explode(' ', $form['cssClass'])) && class_exists('Brownbox\Config\BB_Cart') && isset(Brownbox\Config\BB_Cart::$checkout_form_confirmation_url) && $confirmation['type'] == 'redirect') {
-        $confirmation['url'] = site_url(Brownbox\Config\BB_Cart::$checkout_form_confirmation_url);
+    $url = '';
+    if (in_array('bb_cart_donations', explode(' ', $form['cssClass'])) && class_exists('Brownbox\Config\BB_Cart') && isset(Brownbox\Config\BB_Cart::$donate_form_confirmation_url)) {
+        $url = site_url(Brownbox\Config\BB_Cart::$donate_form_confirmation_url);
+    } elseif (in_array('bb_cart_checkout', explode(' ', $form['cssClass'])) && class_exists('Brownbox\Config\BB_Cart') && isset(Brownbox\Config\BB_Cart::$checkout_form_confirmation_url)) {
+        $url = site_url(Brownbox\Config\BB_Cart::$checkout_form_confirmation_url);
+    }
+    if (!empty($url)) {
+        if (!empty($confirmation['redirect'])) {
+            $query = parse_url($confirmation['redirect'], PHP_URL_QUERY);
+            $url .= '?'.$query;
+        }
+        $confirmation = array(
+                'redirect' => $url,
+        );
     }
     return $confirmation;
 }
