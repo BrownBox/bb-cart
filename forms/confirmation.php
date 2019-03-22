943d638b31b5d61c4981804f834eb2075d300458
Mark Parnell <markparnell@brownbox.net.au>
2019-03-22 13:43:00 +1100

    Reversed confirmation logic to process checkout form first

--- forms/confirmation.php
+++ forms/confirmation.php
@@ -2,10 +2,10 @@
 add_filter('gform_confirmation', 'bb_cart_form_confirmation', 10, 3);
 function bb_cart_form_confirmation($confirmation, $form, $entry) {
     $url = '';
-    if (in_array('bb_cart_donations', explode(' ', $form['cssClass'])) && class_exists('Brownbox\Config\BB_Cart') && isset(Brownbox\Config\BB_Cart::$donate_form_confirmation_url)) {
-        $url = site_url(Brownbox\Config\BB_Cart::$donate_form_confirmation_url);
-    } elseif (in_array('bb_cart_checkout', explode(' ', $form['cssClass'])) && class_exists('Brownbox\Config\BB_Cart') && isset(Brownbox\Config\BB_Cart::$checkout_form_confirmation_url)) {
+    if (in_array('bb_cart_checkout', explode(' ', $form['cssClass'])) && class_exists('Brownbox\Config\BB_Cart') && isset(Brownbox\Config\BB_Cart::$checkout_form_confirmation_url)) {
         $url = site_url(Brownbox\Config\BB_Cart::$checkout_form_confirmation_url);
+    } elseif (in_array('bb_cart_donations', explode(' ', $form['cssClass'])) && class_exists('Brownbox\Config\BB_Cart') && isset(Brownbox\Config\BB_Cart::$donate_form_confirmation_url)) {
+        $url = site_url(Brownbox\Config\BB_Cart::$donate_form_confirmation_url);
     }
     if (!empty($url)) {
         if (!empty($confirmation['redirect'])) {
